6277d59651fac325050112a7987d37ad50ea2ed4
Thanh Tra <97927500+ngthanhtra165@users.noreply.github.com>
2023-03-06 14:55:00 +0700

    update color

--- src/components/ListText/index.js
+++ src/components/ListText/index.js
@@ -4,11 +4,33 @@ import "./index.css"
 
 const ListText = ({ score, arrList}) => {
   const arr_List = arrList;
+  let color = [];
+  let hihi = 0;
+  if(score > 6) {
+    hihi = {
+      backgroundColor: "#019e43"
+    }
+  }
+  else {
+    hihi = {
+      backgroundColor: "#ea1537"
+    }
+  }
   let dodai = arr_List.length;
   let small_score = [0 , 0 , 0 , 0 , 0 , 0];
   let i = 0, sum = 0;
   while(i <= dodai - 2) {
     small_score[i] = parseInt(-1 + Math.random() * 2 + score);
+    if(small_score[i] > 6) {
+      color.push({
+        backgroundColor: "#019e43"
+      })
+    }
+    else {
+      color.push({
+        backgroundColor: "#ea1537"
+      })
+    }
     //console.log(small_score[i]);
     sum += small_score[i];
     i++;
@@ -22,7 +44,7 @@ const ListText = ({ score, arrList}) => {
         item?.name ? (
           <div className="d-flex  align-items-center" key={index}>
             { (score == 0)?
-            <FaQuestionCircle className="flex-shrink-0" /> : (<div className="score-container">
+            <FaQuestionCircle className="flex-shrink-0" /> : (<div className="score-container" style = {color[item?.index]} >
                 {small_score[item?.index]}
             </div>)
             }
@@ -33,7 +55,7 @@ const ListText = ({ score, arrList}) => {
             
             <p className="mb-0 ps-2" style={{ marginRight : "7px"}}>{item?.title}</p>
             { score ? 
-            (<div className="score-container1">
+            (<div className="score-container1" style={hihi}>
                {score}
             </div>) : <FaQuestion style={{ marginBottom : "5px"}}></FaQuestion>
             }
